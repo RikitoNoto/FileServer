563a07fc1e92aea9d53cf0e72fef85fcc4c197f2
RikitoNoto <rikitonoto@gmail.com>
2020-09-13 22:04:17 +0900

    create system file

---
+++ client/clientSystem.py
@@ -0,0 +1,7 @@
+
+
+class ClientSystem:
+
+    #TODO GUIアプリ起動
+    #TODO ボタン配置
+    #TODO ボタンクリックイベント
---
+++ server/serverSystem.py
@@ -0,0 +1,7 @@
+
+
+class ServerSystem:
+    #TODO サーバーを開く
+    #TODO パスを受信する機能
+    #TODO パスがディレクトリなら構造を返す
+    #TODO パスがファイルならファイルの内容を返す
